173a799ed4b8dbb3e601cac7b3b0437c2334b151
alejo albornoz <ale-00-@hotmail.com>
2024-05-14 13:00:33 -0300

    intento de responsive 2

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -4,25 +4,21 @@ import "../css/Main.css";
 import useStore from "../store/useStore";
 
 const Main = () => {
-
-
-  const { modoOscuro, estiloClaro, estiloOscuro } = useStore()
+  const { modoOscuro, estiloClaro, estiloOscuro } = useStore();
 
   return (
     <>
       <Container id="section1" className="aboutme">
         <Row>
           <Col md={6}>
-            <div style={modoOscuro?estiloOscuro:estiloClaro}>
-            <h2 className="mb-4">
-              Sobre mí
-            </h2>
-            <h3>
-              Soy estudiante de la Tecnicatura Universitaria en
-              Programación dictado en la UTN FRT en la cual me encuentro
-              cursando el 2do año. Mi objetivo es poder graduarme y al mismo
-              tiempo conseguir experiencia en el ambiente laboral IT
-            </h3>
+            <div style={modoOscuro ? estiloOscuro : estiloClaro}>
+              <h2 className="mb-4">Sobre mí</h2>
+              <h3>
+                Soy estudiante de la Tecnicatura Universitaria en Programación
+                dictado en la UTN FRT en la cual me encuentro cursando el 2do
+                año. Mi objetivo es poder graduarme y al mismo tiempo conseguir
+                experiencia en el ambiente laboral IT
+              </h3>
             </div>
           </Col>
           <Col md={{ span: 2, offset: 2 }}>
@@ -36,92 +32,101 @@ const Main = () => {
         </Row>
       </Container>
 
-      <Container id="section2" className="skills d-flex container-fluid">
-        <div style={modoOscuro?estiloOscuro:estiloClaro}>
-        <h2 className="mb-5" >
-          Skills
-        </h2>
+     
+        <Container className="d-flex text-center">
+       
+          <Row className="d-flex container-fluid">
+          <div id="section2" className="text-center skills" style={modoOscuro ? estiloOscuro : estiloClaro}>
+          <h2 className="mb-5">Skills</h2>
         </div>
-        <Container className="d-flex">
-        <Row>
-        <CardGroup className="group1">
-          <Col md={4}>
-            <Card text={modoOscuro? "white" : "black"} style={modoOscuro ? estiloOscuro : estiloClaro}>
-              <Card.Img
-                variant="top"
-                src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/38/HTML5_Badge.svg/800px-HTML5_Badge.svg.png"
-              />
-              <Card.Body>
-                <Card.Title>HTML</Card.Title>
-              </Card.Body>
-            </Card>
-          </Col>
-          <Col md={4}>
-          <Card text={modoOscuro? "white" : "black"} style={modoOscuro ? estiloOscuro : estiloClaro}>
-              <Card.Img
-                variant="top"
-                src="https://cdn.icon-icons.com/icons2/1826/PNG/512/4202020css3htmllogosocialsocialmedia-115668_115633.png"
-              />
-              <Card.Body>
-                <Card.Title>CSS</Card.Title>
-              
-              </Card.Body>
-            </Card>
-          </Col>
-          <Col md={4}>
-          <Card text={modoOscuro? "white" : "black"} style={modoOscuro ? estiloOscuro : estiloClaro}>
-              <Card.Img
-                variant="top"
-                src="https://cdn.icon-icons.com/icons2/2108/PNG/512/javascript_icon_130900.png"
-              />
-              <Card.Body>
-                <Card.Title>JavaScript</Card.Title>
-               
-              </Card.Body>
-            </Card>
-          </Col>
-          </CardGroup>
-          <CardGroup className="group2">
-          <Col md={4}>
-          <Card text={modoOscuro? "white" : "black"} style={modoOscuro ? estiloOscuro : estiloClaro}>
-              <Card.Img
-                variant="top"
-                src="https://cdn.icon-icons.com/icons2/2415/PNG/512/react_original_logo_icon_146374.png"
-              />
-              <Card.Body>
-                <Card.Title>React</Card.Title>
-               
-              </Card.Body>
-            </Card>
-          </Col>
-          <Col md={4}>
-          <Card text={modoOscuro? "white" : "black"} style={modoOscuro ? estiloOscuro : estiloClaro}>
-              <Card.Img
-                variant="top"
-                src="https://cdn-icons-png.flaticon.com/512/4492/4492311.png"
-              />
-              <Card.Body>
-                <Card.Title>Sql Server</Card.Title>
-               
-              </Card.Body>
-            </Card>
-          </Col>
-          <Col md={4}>
-          <Card text={modoOscuro? "white" : "black"} style={modoOscuro ? estiloOscuro : estiloClaro}>
-              <Card.Img
-                variant="top"
-                src="https://upload.wikimedia.org/wikipedia/commons/4/4f/Csharp_Logo.png"
-              />
-              <Card.Body>
-                <Card.Title>C#</Card.Title>
-               
-              </Card.Body>
-            </Card>
-          </Col>
-          </CardGroup>
-        </Row>
-      </Container>
-      </Container>
+            {/* <CardGroup className="group1"> */}
+              <Col className="d-flex justify-content-center">
+                <Card
+                  text={modoOscuro ? "white" : "black"}
+                  style={modoOscuro ? estiloOscuro : estiloClaro}
+                >
+                  <Card.Img
+                    variant="top"
+                    src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/38/HTML5_Badge.svg/800px-HTML5_Badge.svg.png"
+                  />
+                  <Card.Body>
+                    <Card.Title>HTML</Card.Title>
+                  </Card.Body>
+                </Card>
+              </Col>
+              <Col className="d-flex justify-content-center" >
+                <Card
+                  text={modoOscuro ? "white" : "black"}
+                  style={modoOscuro ? estiloOscuro : estiloClaro}
+                >
+                  <Card.Img
+                    variant="top"
+                    src="https://cdn.icon-icons.com/icons2/1826/PNG/512/4202020css3htmllogosocialsocialmedia-115668_115633.png"
+                  />
+                  <Card.Body>
+                    <Card.Title>CSS</Card.Title>
+                  </Card.Body>
+                </Card>
+              </Col>
+              <Col className="d-flex justify-content-center">
+                <Card
+                  text={modoOscuro ? "white" : "black"}
+                  style={modoOscuro ? estiloOscuro : estiloClaro}
+                >
+                  <Card.Img
+                    variant="top"
+                    src="https://cdn.icon-icons.com/icons2/2108/PNG/512/javascript_icon_130900.png"
+                  />
+                  <Card.Body>
+                    <Card.Title>JavaScript</Card.Title>
+                  </Card.Body>
+                </Card>
+              </Col>
+            {/* <CardGroup className="group2"> */}
+              <Col className="d-flex justify-content-center">
+                <Card
+                  text={modoOscuro ? "white" : "black"}
+                  style={modoOscuro ? estiloOscuro : estiloClaro}
+                >
+                  <Card.Img
+                    variant="top"
+                    src="https://cdn.icon-icons.com/icons2/2415/PNG/512/react_original_logo_icon_146374.png"
+                  />
+                  <Card.Body>
+                    <Card.Title>React</Card.Title>
+                  </Card.Body>
+                </Card>
+              </Col>
+              <Col className="d-flex justify-content-center">
+                <Card
+                  text={modoOscuro ? "white" : "black"}
+                  style={modoOscuro ? estiloOscuro : estiloClaro}
+                >
+                  <Card.Img
+                    variant="top"
+                    src="https://cdn-icons-png.flaticon.com/512/4492/4492311.png"
+                  />
+                  <Card.Body>
+                    <Card.Title>Sql Server</Card.Title>
+                  </Card.Body>
+                </Card>
+              </Col>
+              <Col className="d-flex justify-content-center">
+                <Card
+                  text={modoOscuro ? "white" : "black"}
+                  style={modoOscuro ? estiloOscuro : estiloClaro}
+                >
+                  <Card.Img
+                    variant="top"
+                    src="https://upload.wikimedia.org/wikipedia/commons/4/4f/Csharp_Logo.png"
+                  />
+                  <Card.Body>
+                    <Card.Title>C#</Card.Title>
+                  </Card.Body>
+                </Card>
+              </Col>
+          </Row>
+        </Container>
     </>
   );
 };
