aa5b2e32c906f17c7986b0a77996227cdd5808b5
Alejo <aalejoo12@gmail.com>
2024-05-22 10:17:10 -0300

    animaciones varias

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,34 +1,50 @@
 import React from "react";
+import { useState } from "react";
 import { Container, Row, Col, Card, CardGroup } from "react-bootstrap";
 import "../css/Main.css";
 import useStore from "../store/useStore";
 import { Typewriter } from "react-simple-typewriter";
-import { motion} from "framer-motion";
+import { motion } from "framer-motion";
+
+
 
 const Main = () => {
+
   const { modoOscuro, estiloClaro, estiloOscuro } = useStore();
 
+ 
+
+
+
+  
   return (
     <>
       <Container id="section1" className="aboutme">
         <Row>
           <Col md={6}>
             <div style={modoOscuro ? estiloOscuro : estiloClaro}>
+            <motion.div
+                  whileInView={{x:[0,150,0]}}
+                  transition={{
+                    ease: "linear",
+                    duration: 2,
+                    x: { duration: 1 }
+                  }}
+                >
               <h2 className="mb-4">Sobre mí</h2>
               <h3>
-
-                <Typewriter
-                  words={[
-                    "Soy estudiante de la Tecnicatura Universitaria en Programación dictado en la UTN FRT en la cual me encuentro cursando el 2do año. Mi objetivo es poder graduarme y al mismo tiempo conseguir experiencia en el ambiente laboral IT",
-                  ]}
-                  loop={false} // Puedes configurar si quieres que se repita la animación
-                  cursor
-                  cursorStyle="_"
-                  typeSpeed={8} // Velocidad de escritura en milisegundos
-                  deleteSpeed={2} // Velocidad de borrado en milisegundos
-                  delaySpeed={8000} // Tiempo de espera entre escritura y borrado en milisegundos
-                />
-                
+               
+                  <Typewriter
+                    words={[
+                      "Soy estudiante de la Tecnicatura Universitaria en Programación dictado en la UTN FRT en la cual me encuentro cursando el 2do año. Mi objetivo es poder graduarme y al mismo tiempo conseguir experiencia en el ambiente laboral IT",
+                    ]}
+                    loop={false} // Puedes configurar si quieres que se repita la animación
+                    cursor
+                    cursorStyle="_"
+                    typeSpeed={7} // Velocidad de escritura en milisegundos
+                    deleteSpeed={2} // Velocidad de borrado en milisegundos
+                    delaySpeed={8000} // Tiempo de espera entre escritura y borrado en milisegundos
+                  />
               </h3>
               {/* <h3>
                 Soy estudiante de la Tecnicatura Universitaria en Programación
@@ -36,14 +52,17 @@ const Main = () => {
                 año. Mi objetivo es poder graduarme y al mismo tiempo conseguir
                 experiencia en el ambiente laboral IT
               </h3> */}
+                </motion.div>
+
             </div>
           </Col>
           <Col md={{ span: 2, offset: 2 }}>
             <a href="https://frt.cvg.utn.edu.ar/" target="blank">
+          
               <img
                 src="https://pbs.twimg.com/profile_images/998209251339259904/8S8TSIFi_400x400.jpg"
                 className="logoUTN"
-              />
+                />
             </a>
           </Col>
         </Row>
@@ -59,42 +78,42 @@ const Main = () => {
             <h2 className="mb-5">Skills</h2>
           </div>
           {/* <CardGroup className="group1"> */}
-         
+
           <Col className="d-flex justify-content-center">
-          <motion.div initial={{ scale: 0 }}
-          whileInView={{ scale: 1 }}
-          transition={{duration:0.5}}>
-            <Card
-              text={modoOscuro ? "white" : "black"}
-              style={modoOscuro ? estiloOscuro : estiloClaro}
-            >
-              <Card.Img
-                variant="top"
-                src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/38/HTML5_Badge.svg/800px-HTML5_Badge.svg.png"
-              />
-              <Card.Body>
-                <Card.Title>HTML</Card.Title>
-              </Card.Body>
-            </Card>
-          </motion.div>
+            <motion.div initial={{ scale: 0 }}
+              whileInView={{ scale: 1 }}
+              transition={{ duration: 0.5 }}>
+              <Card
+                text={modoOscuro ? "white" : "black"}
+                style={modoOscuro ? estiloOscuro : estiloClaro}
+              >
+                <Card.Img
+                  variant="top"
+                  src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/38/HTML5_Badge.svg/800px-HTML5_Badge.svg.png"
+                />
+                <Card.Body>
+                  <Card.Title>HTML</Card.Title>
+                </Card.Body>
+              </Card>
+            </motion.div>
 
           </Col>
           <Col className="d-flex justify-content-center">
-          <motion.div initial={{ scale: 0 }}
-          whileInView={{ scale: 1 }}
-          transition={{duration:0.5}}>
-            <Card
-              text={modoOscuro ? "white" : "black"}
-              style={modoOscuro ? estiloOscuro : estiloClaro}
-            >
-              <Card.Img
-                variant="top"
-                src="https://cdn.icon-icons.com/icons2/1826/PNG/512/4202020css3htmllogosocialsocialmedia-115668_115633.png"
-              />
-              <Card.Body>
-                <Card.Title>CSS</Card.Title>
-              </Card.Body>
-            </Card>
+            <motion.div initial={{ scale: 0 }}
+              whileInView={{ scale: 1 }}
+              transition={{ duration: 0.5 }}>
+              <Card
+                text={modoOscuro ? "white" : "black"}
+                style={modoOscuro ? estiloOscuro : estiloClaro}
+              >
+                <Card.Img
+                  variant="top"
+                  src="https://cdn.icon-icons.com/icons2/1826/PNG/512/4202020css3htmllogosocialsocialmedia-115668_115633.png"
+                />
+                <Card.Body>
+                  <Card.Title>CSS</Card.Title>
+                </Card.Body>
+              </Card>
             </motion.div>
           </Col>
           <Col className="d-flex justify-content-center">
